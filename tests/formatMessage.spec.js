d9b67df58bc5366c0428bb7a4a8f46b7e899afa7
Gerald Yeo <gerald@fusedthought.com>
2017-01-21 22:50:23 +0800

    increase coverage for formatMessage

--- tests/formatMessage.spec.js
+++ tests/formatMessage.spec.js
@@ -1,4 +1,5 @@
 import {expect} from 'chai';
+import {stub} from 'sinon';
 import formatMessage from '../src/formatMessage';
 
 describe('formatMessage', function () {
@@ -56,6 +57,50 @@ describe('formatMessage', function () {
     expect(result).to.throw(Error);
   });
 
+  it('returns message directly (ENV:production, values empty)', function () {
+    const values = rewireEnv();
+    const state = getState();
+    const result = formatMessage(state)({
+      id: 'test'
+    });
+
+    testResult(result, values, 'let us test');
+    resetEnv();
+  });
+
+  it('returns defaultMessage directly (ENV:production, values empty)', function () {
+    const values = rewireEnv();
+    const result = formatMessage({})({
+      id: 'test',
+      defaultMessage: 'it is missing'
+    });
+
+    testResult(result, values, 'it is missing');
+    resetEnv();
+  });
+
+  it('returns id directly (ENV:production, values empty)', function () {
+    const values = rewireEnv();
+    const result = formatMessage({})({
+      id: 'test',
+    });
+
+    testResult(result, values, 'test');
+    resetEnv();
+  });
+
+  it('returns id when invariant is disabled with no message/defaultMessage', function () {
+    const invariant = stub();
+    formatMessage.__Rewire__('invariant', invariant);
+
+    const result = formatMessage({})({
+      id: 'test',
+    });
+    expect(result).to.equal('test');
+    expect(invariant.called).to.be.true;
+    formatMessage.__ResetDependency__('invariant');
+  });
+
   function getState() {
     return {
       locale: 'en',
@@ -64,5 +109,28 @@ describe('formatMessage', function () {
         testvar: 'let us replace {s}'
       }
     };
+  }
+
+  function rewireEnv() {
+    const values = {
+      invariant: stub(),
+      template: stub()
+    }
+    formatMessage.__Rewire__('ENV', 'production');
+    formatMessage.__Rewire__('invariant', values.invariant);
+    formatMessage.__Rewire__('template', values.template);
+    return values;
+  }
+
+  function resetEnv() {
+    formatMessage.__ResetDependency__('ENV');
+    formatMessage.__ResetDependency__('invariant');
+    formatMessage.__ResetDependency__('template');
+  }
+
+  function testResult(result, values, str) {
+    expect(result).to.equal(str);
+    expect(values.invariant.called).to.be.false;
+    expect(values.template.called).to.be.false;
   }
 });
